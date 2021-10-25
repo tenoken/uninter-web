0bdf4e50659c3aedc953db5150746823c7c54ea0
tenoken <shiroiparadina@gmail.com>
2021-10-24 23:42:15 -0300

    color changes

--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -20,11 +20,11 @@ export class StudentComponent implements OnInit{
   public students: any = [];
 
   buildStudentList() {
-    let student1 = new Student(1234567, "Carlos","Marketing Digital", new Date(1990,2,18));
-    let student2 = new Student(1234567, "Ricardo","Administração", new Date(1990,2,18));
-    let student3 = new Student(1234567, "Aline","Nutrição", new Date(1990,2,18));
-    let student4 = new Student(3294451, "Gustavo","Análise e Desennvolvimento de Sistemas", new Date(1994,2,18));
-    let student5 = new Student(1234567, "Vanessa","Desing Gráfico", new Date(1990,2,18));
+    let student1 = new Student(3294451, "Gustavo","Análise e Desennvolvimento de Sistemas", new Date(1997,9,13));
+    let student2 = new Student(4214512, "Ricardo","Administração", new Date(1990,2,18));
+    let student3 = new Student(3451455, "Aline","Nutrição", new Date(1990,2,18));
+    let student4 = new Student(1821385, "Carlos","Marketing Digital", new Date(1990,2,18));
+    let student5 = new Student(2545486, "Vanessa","Desing Gráfico", new Date(1990,2,18));
 
     this._studentList.push(student1);
     this._studentList.push(student2);
